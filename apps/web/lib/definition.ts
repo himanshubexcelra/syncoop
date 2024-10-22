7654d690b92c306741d69cbebfc227ad27daddec
Akash Sharma <akash.sharma@Excelra.com>
2024-10-22 15:44:43 +0530

    Dev - Integrated library package for pathways

--- apps/web/lib/definition.ts
+++ apps/web/lib/definition.ts
@@ -335,3 +335,18 @@ export interface ProjectAccordionType {
   dataCreate: UserData,
   roleType: string | undefined,
 }
+
+export interface LibraryDataNode{
+  id: string;
+  type?: string;
+  smiles?: string;
+  parentId?: string;
+  score?: number | null;
+  name?: string;
+  condition?: string;
+  reactionCount?: number;
+  doi?: string;
+  isRegulated?: boolean;
+  isProtected?: boolean;
+  isInInventory?: boolean;
+}
